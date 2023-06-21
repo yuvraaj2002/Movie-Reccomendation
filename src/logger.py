dfa81244328256d95be588e2e646936ac7d3ece2
Yuvraj Singh <ys2002github@gmail.com>
2023-06-21 12:54:22 +0530

    Logger file for creating logs created

---
+++ src/logger.py
@@ -0,0 +1,17 @@
+import os
+import logging
+from datetime import datetime
+
+
+# Name of log file
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(),"Logs",LOG_FILE) # Path where to store logs
+os.makedirs(logs_path,exist_ok= True)
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+
+# Basic configuration
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
